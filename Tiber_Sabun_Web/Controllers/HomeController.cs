0a0d5a348b2df8374379d664c1d25e880e7513e7
Mustafa <mustafakalender1989@gmail.com>
2022-08-20 23:13:27 +0300

    Add project files.

---
+++ Tiber_Sabun_Web/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Tiber_Sabun_Web.Models;
+
+namespace Tiber_Sabun_Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Urunler()
+        {
+            return View();
+        }
+
+        public IActionResult UrunDetay()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
